bd8602c1fb4da20868c9e197d3305f1ee91184c1
Kenzie Togami <ket1999@gmail.com>
2018-04-17 16:22:40 -0700

    Complete installation bootstrap script

---
+++ main.py
@@ -0,0 +1,13 @@
+import vsi
+from blessings import Terminal
+
+t = Terminal()
+
+
+def main():
+    print(t.green('Welcome to Very Shell Installer (VSI)!'))
+    print(t.green("Let's get started."))
+
+
+if __name__ == '__main__':
+    main()
--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ _vsi_read_os_release_field () {
 }
 
 _vsi_echo () {
-    printf "%s\n" "$1" >&2
+    printf "[VSI] %s\n" "$1" >&2
 }
 
 _vsi_is_cmd_present () {
@@ -32,21 +32,56 @@ _vsi_env_default () {
     fi
 }
 
+_vsi_install () {
+    _vsi_echo "Installing..."
+    _vsi_echo ""
+
+    _vsi_echo "Cloning repository ${GITHUB_OWNER}/${GITHUB_NAME} to $INSTALL_FOLDER..."
+    git clone https://github.com/"$GITHUB_OWNER"/"$GITHUB_NAME" .
+    _vsi_echo "Checking out ref $GITHUB_REF..."
+    git checkout "$GITHUB_REF"
+
+    _vsi_echo "Installing virtualenv to $INSTALL_FOLDER..."
+    git clone --depth 1 https://github.com/pypa/virtualenv "$INSTALL_FOLDER/.virtualenv-pip"
+    _vsi_echo "Creating virtual environment..."
+    python3 "$INSTALL_FOLDER/.virtualenv-pip/virtualenv.py" -p python3 "$INSTALL_FOLDER/.venv"
+    _vsi_echo "Activating virtual environment..."
+    . "$INSTALL_FOLDER/.venv/bin/activate"
+    _vsi_echo "Installing dependencies..."
+    pip3 install -r requirements.txt
+    _vsi_echo "Running installer..."
+    exec python3 ./main.py
+}
+
 
 ### Configuration
 _vsi_env_default XDG_CONFIG_HOME "$HOME/.config"
 _vsi_env_default INSTALL_FOLDER "$XDG_CONFIG_HOME/very-shell-installer"
 
+_vsi_env_default GITHUB_OWNER "kenzierocks"
+_vsi_env_default GITHUB_NAME "very-shell-installer"
+_vsi_env_default GITHUB_REF "master"
+
 ### Actual script
+
+if [ ! -d "$INSTALL_FOLDER" ]; then
+    _vsi_echo "Creating installation folder at $INSTALL_FOLDER..."
+    mkdir -p "$INSTALL_FOLDER"
+fi
+cd "$INSTALL_FOLDER"
+
+if { _vsi_is_cmd_present git && _vsi_is_cmd_present python3 && _vsi_is_cmd_present pip3 ; }; then
+    # Requirements satisfied. Go right ahead.
+    _vsi_echo "Requirements already satisfied."
+    _vsi_install
+    exit 0
+fi
 
 # Requirements
 if ! _vsi_is_cmd_present curl; then
     _vsi_echo "cURL is required to run this script."
     exit 2
 fi
-
-_vsi_echo "Creating installation folder at $INSTALL_FOLDER..."
-mkdir -p "$INSTALL_FOLDER"
 
 # Generic OS detection
 MAC=mac
@@ -115,11 +150,13 @@ script_file="$INSTALL_FOLDER/$script_id.sh"
 
 if [ ! -e "$script_file" ]; then
     curl -# -s --fail \
-        https://raw.githubusercontent.com/kenzierocks/very-shell-installer/master/setup/"$script_id".sh \
+        https://raw.githubusercontent.com/"$GITHUB_OWNER"/"$GITHUB_NAME"/"$GITHUB_REF"/setup/"$script_id".sh \
             >"$script_file" \
         || { _vsi_echo "Failed to fetch script."; rm -f "$script_file" || true; exit 1; }
 fi
 
+_vsi_echo "Running script..."
 chmod +x "$script_file"
 "$script_file" "${script_args[@]}"
 
+_vsi_install
